280d3e75d7ba8aa8ebc562e58877f6d50d633770
procub3r <procub3r@proton.me>
2024-06-25 23:42:34 +0530

    Add RepCl.advance()

--- repcl.py
+++ repcl.py
@@ -1,4 +1,3 @@
-import math
 import time
 
 '''
@@ -25,11 +24,50 @@ class RepCl:
 
         # counters in a (key: val) format.
         # (proc_id: counter of process `proc_id` with respect to this process)
-        self.counters = {proc_id: 0}  # initialize counter of this process to 0
+        self.counters = {}
 
     def __repr__(self) -> str:
         return f'RepCl(proc_id={self.proc_id}, epoch={self.epoch}, offsets={self.offsets}, counters={self.counters})'
 
+    '''
+    Use system time and calculate the current epoch as
+    (unix timestamp in ms) / interval
+    '''
     @staticmethod
     def get_current_epoch(interval: int) -> int:
         return int(time.time() * 1000 / interval)
+
+    '''
+    Advance the clock by one timestep
+    '''
+    def advance(self) -> None:
+        # calculate the current epoch
+        new_epoch = max(self.get_current_epoch(self.interval), self.epoch)
+
+        # if the event occured in the same epoch as the last event
+        if self.epoch == new_epoch:
+            if self.proc_id in self.counters:
+                # increment this process's counter if it already exists
+                self.counters[self.proc_id] += 1
+            else:
+                # or create a counter for this process and start it off with 0
+                self.counters[self.proc_id] = 0
+
+        # if the event occurred in a different epoch as the last event
+        else:
+            # delete all counters because a new epoch was entered
+            self.counters.clear()
+
+            # loop through all the offsets
+            for p in list(self.offsets.keys()):
+                # re-compute the offset with the new epoch
+                new_offset = self.offsets[p] + new_epoch - self.epoch  # calculate the new offset
+                if new_offset >= self.epsilon:
+                    # remove the offset if it is greater than epsilon
+                    del self.offsets[p]
+                else:
+                    # store the re-computed offset if it is still under epsilon
+                    self.offsets[p] = new_offset
+
+        self.epoch = new_epoch  # update the epoch
+        self.offsets[self.proc_id] = 0  # must always remain 0
