50766eb39cbbf0075cd52e39b8fad70a3eeae138
procub3r <procub3r@proton.me>
2024-07-03 19:56:21 +0530

    Fix some errors

--- repcl.py
+++ repcl.py
@@ -26,7 +26,7 @@ class RepCl:
     @staticmethod
     def extract(number, k, p):
         return ((1 << k) - 1) & (number >> p)
-    
+
     def remove_offset_at_index(self, index):
         # 010 011 101
         #   2   1   0
@@ -59,16 +59,16 @@ class RepCl:
             else:
                 self.set_offset_at_index(index, new_offset)
                 self.offset_bmp = self.offset_bmp | (1 << process_id)
-            
+
             bitmap = bitmap & (bitmap - 1)
             index += 1
-        
+
         self.hlc = new_hlc
 
     def set_offset_at_index(self, index, new_offset):
         if new_offset > (1 << self.bits_per_offset) - 1:
             raise ValueError('Offset value too large')
-        
+
         mask = np.uint64((1 << self.bits_per_offset) - 1) << (index * self.bits_per_offset)
         mask = ~mask
 
@@ -81,7 +81,7 @@ class RepCl:
 
     def send_local(self) -> None:
         new_hlc = math.max(self.hlc, self.get_current_epoch())
-        new_offset = new_hlc - self.node_hlc
+        new_offset = new_hlc - self.hlc
         offset_at_pid = self.get_offset_at_index(self.proc_id)
 
         if (new_hlc == self.hlc and offset_at_pid <= new_offset):
@@ -90,16 +90,16 @@ class RepCl:
             new_offset = math.min(new_offset, offset_at_pid)
 
             index = 0
-            bitmap = self.offset_bmp    
+            bitmap = self.offset_bmp
             while (bitmap > 0):
-                process_id = math.log2((~(bitmap ^ (~(bitmap - 1))) + 1) >> 1)
+                process_id = int(math.log2((~(bitmap ^ (~(bitmap - 1))) + 1) >> 1))
                 if (process_id == self.proc_id):
                     self.set_offset_at_index(index, new_offset)
                     self.offset_bmp = self.offset_bmp | (1 << process_id)
-                
+
                 bitmap = bitmap & (bitmap - 1)
                 index += 1
-            
+
             self.counters = 0
             self.offset_bmp = self.offset_bmp | (1 << self.proc_id)
         else:
@@ -109,7 +109,7 @@ class RepCl:
             index = 0
             bitmap = self.offset_bmp
             while (bitmap > 0):
-                process_id = math.log2((~(bitmap ^ (~(bitmap - 1))) + 1) >> 1)
+                process_id = int(math.log2((~(bitmap ^ (~(bitmap - 1))) + 1) >> 1))
                 if (process_id == self.proc_id):
                     self.set_offset_at_index(index, 0)
                     self.offset_bmp = self.offset_bmp | (1 << process_id)
